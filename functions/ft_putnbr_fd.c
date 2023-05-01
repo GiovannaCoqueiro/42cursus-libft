ce5993bfe2f1949f90808135f9494c47f9c9b530
Giovanna Coqueiro <gcoqueir@c1r2p3.42sp.org.br>
2023-05-01 15:16:57 -0300

    putnbr_fd

--- functions/ft_putnbr_fd.c
+++ functions/ft_putnbr_fd.c
@@ -1,6 +1,29 @@
-// #include "libft.h"
+#include "libft.h"
 
-// void	ft_putnbr_fd(int n, int fd)
-// {
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	a;
 
-// }
+	if (n == -2147483648)
+	{
+		write(fd, "-", 1);
+		ft_putnbr_fd(214748364, fd);
+		write(fd, "8", 1);
+	}
+	else if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		a = (n % 10 + '0');
+		write(fd, &a, 1);
+	}
+	else if (n >= 0 && n <= 9)
+	{
+		a = (n + '0');
+		write(fd, &a, 1);
+	}
+	else
+	{
+		write(fd, "-", 1);
+		ft_putnbr_fd(n * -1, fd);
+	}
+}
