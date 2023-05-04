e31d5040103d358c7dabcec03e39bbe60d1b1b59
Giovanna Coqueiro <gcoqueir@c1r2p3.42sp.org.br>
2023-05-04 11:35:03 -0300

    strlen

--- functions/ft_strlen.c
+++ functions/ft_strlen.c
@@ -6,9 +6,11 @@
 /*   By: gcoqueir <gcoqueir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 14:07:46 by gcoqueir          #+#    #+#             */
-/*   Updated: 2023/05/04 09:47:10 by gcoqueir         ###   ########.fr       */
+/*   Updated: 2023/05/04 11:33:38 by gcoqueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 size_t	ft_strlen(const char *str)
 {
