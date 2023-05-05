3a012fd535be3cf266e20eb05722f3c9c5731000
Giovanna Coqueiro <gcoqueir@c1r2p3.42sp.org.br>
2023-05-05 09:20:07 -0300

    General Update

--- functions/ft_memcmp.c
+++ functions/ft_memcmp.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcoqueir <gcoqueir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/03 14:06:40 by gcoqueir          #+#    #+#             */
-/*   Updated: 2023/05/03 14:06:41 by gcoqueir         ###   ########.fr       */
+/*   Created: 2023/05/05 09:18:07 by gcoqueir          #+#    #+#             */
+/*   Updated: 2023/05/05 09:18:08 by gcoqueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const char	*p1;
-	const char	*p2;
-	size_t		c;
+	const unsigned char	*p1;
+	const unsigned char	*p2;
+	size_t				c;
 
-	if (n == 0)
+	if (n <= 0)
 		return (0);
-	p1 = (const char *)s1;
-	p2 = (const char *)s2;
+	p1 = (const unsigned char *)s1;
+	p2 = (const unsigned char *)s2;
 	c = 0;
 	while (c < n - 1 && p1[c] == p2[c])
 		c++;
-	return (p2[c] - p1[c]);
+	return (p1[c] - p2[c]);
 }
