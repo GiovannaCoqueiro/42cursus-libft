5564c523e3b93b0f45280a206832506c7ccf2137
Giovanna Coqueiro <gcoqueir@c1r2p3.42sp.org.br>
2023-05-12 10:51:38 -0300

    calloc

--- functions/ft_calloc.c
+++ functions/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: gcoqueir <gcoqueir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 09:17:41 by gcoqueir          #+#    #+#             */
-/*   Updated: 2023/05/11 15:58:49 by gcoqueir         ###   ########.fr       */
+/*   Updated: 2023/05/12 10:49:53 by gcoqueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*arr;
 
-	if (nmemb * size == 0 || nmemb > 2147483627 / size)
+	if (nmemb == 0 || size == 0 || nmemb > 2147483627 / size)
 		return (NULL);
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
